7bf53c8be2e6386151c86ca2f8fa745ae878bf46
Harekam Singh <harekam.singh@COMVIVA.COM>
2017-10-04 08:41:12 +0530

    resume and about me updated

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $(function() {
     $("#about-me").typed({
-        strings: ["Tech Enthusiast.", "Senior Engineer at Mahindra Comviva.", "System Developer | Designer."],
+        strings: ["Tech Enthusiast.", "Senior Engineer at Mahindra Comviva.", "Software Developer | Designer."],
         typeSpeed: 30,
         loop: true,
     });
